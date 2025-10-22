eedd4fecdfd77b9e4d8840443db6c68b52abfab4
okency1 <okohchristopher1027@gmail.com>
2025-10-22 02:40:43 +0100

    mywhu

---
+++ countcharacter/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func CountCharacter(s string, c rune) int {
+	if s == "" {
+		return 0
+	}
+	count := 0
+	for _, i := range s {
+		if i == c {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(CountCharacter("", 'l'))
+}
