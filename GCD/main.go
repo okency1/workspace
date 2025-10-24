e794d6575f10faff5b6d0b2d9be3eaf12db0eba8
cokoh <etzkristokency2@gmail.com>
2025-10-24 02:59:12 +0100

    ftkug

--- GCD/main.go
+++ GCD/main.go
@@ -3,15 +3,23 @@ package main
 import "fmt"
 
 func gcd(a, b uint) uint {
-	if a == 0 || b == 0 {
-		return 0
-		for b != 0 {
-			a, b = b, a%b
-		}
+	// If either number is zero, return the other (standard GCD behavior)
+	if a == 0 {
+		return b
+	}
+	if b == 0 {
 		return a
 	}
-}
 
+	// Euclidean algorithm for GCD
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
 func main() {
-	fmt.Println(gcd(12, 6))
+	fmt.Println(Gcd(42, 10))
+	fmt.Println(Gcd(42, 12))
+	fmt.Println(Gcd(14, 77))
+	fmt.Println(Gcd(17, 3))
 }
