e7709bc1255e531dde069078226b50359bd9af8a
cokoh <etzkristokency2@gmail.com>
2025-10-28 15:50:34 +0100

    myfiles

---
+++ digitlen/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func Digitlen(n, base int) int {
+	if base < 2 || base > 36 {
+		return -1
+	}
+	if n < 0 {
+		n = -n
+	}
+	if n == 0 {
+		return 1
+	}
+	count := 0
+	for n > 0 {
+		n = n / base
+		count++
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(Digitlen(100, 10))
+	fmt.Println(Digitlen(100, 2))
+	fmt.Println(Digitlen(-100, 16))
+	fmt.Println(Digitlen(100, -1))
+}
