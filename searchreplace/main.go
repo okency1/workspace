0bd871dae4035ccfe3b5d0a462fadac107c27dbf
okency1 <etzkristokency2@gmail.com>
2025-10-29 22:39:15 +0100

    myfik

--- searchreplace/main.go
+++ searchreplace/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"os"
-	"strings"
+
+	"github.com/01-edu/z01"
 )
 
 func main() {
 	if len(os.Args) != 4 {
 		return
 	}
-	s := os.Args[1]
-	oldi := os.Args[2]
-	newi := os.Args[3]
-	result := strings.ReplaceAll(s, oldi, newi)
-	os.Stdout.WriteString(result + "\n")
+
+	str := os.Args[1]
+	old := os.Args[2]
+	new := os.Args[3]
+
+	if len(old) != 1 || len(new) != 1 {
+		return
+	}
+
+	oldRune := []rune(old)[0]
+	newRune := []rune(new)[0]
+
+	for _, ch := range str {
+		if ch == oldRune {
+			z01.PrintRune(newRune)
+		} else {
+			z01.PrintRune(ch)
+		}
+	}
+	z01.PrintRune('\n') // Always print newline at the end
 }
