448d34b0f888d3e4d154cf11a021d99786d6fa14
okency1 <etzkristokency2@gmail.com>
2025-10-29 22:49:17 +0100

    myfik

--- GCD/main.go
+++ GCD/main.go
@@ -2,21 +2,13 @@ package main
 
 import "fmt"
 
-func gcd(a, b uint) uint {
-	// If either number is zero, return the other (standard GCD behavior)
-	if a == 0 {
-		return b
-	}
+func Gcd(a, b uint) uint {
 	if b == 0 {
 		return a
 	}
-
-	// Euclidean algorithm for GCD
-	for b != 0 {
-		a, b = b, a%b
-	}
-	return a
+	return Gcd(b, a%b)
 }
+
 func main() {
 	fmt.Println(Gcd(42, 10))
 	fmt.Println(Gcd(42, 12))
