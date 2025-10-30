d73d8f3cd770afb2a306ab5e42714a6029837695
okency1 <etzkristokency2@gmail.com>
2025-10-30 02:49:12 +0100

    hky

--- hashcode/main.go
+++ hashcode/main.go
@@ -1,17 +1,13 @@
-package main
+package piscine
 
 func HashCode(dec string) string {
-	size := len(dec)
-	result := make([]byte, size)
-
-	for i := 0; i < size; i++ {
-		hashChar := (int(dec[i]) + size) % 127
-		if hashChar < 33 {
-			hashChar += 33
+	result := ""
+	for _, i := range dec {
+		hash := (int(i) + len(dec)) % 127
+		if hash < 33 {
+			hash += 33
 		}
-
-		result[i] = byte(hashChar)
+		result += string(rune(hash))
 	}
-
-	return string(result)
+	return result
 }
